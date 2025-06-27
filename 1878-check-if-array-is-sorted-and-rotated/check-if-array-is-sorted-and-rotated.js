d9bac630f13cde7962336386650d1cacc7ff542d
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-06-27 14:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (11.48%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let count = 0, n = nums.length;
+    for(let i=0;i<n;i++){
+        if(nums[i]>nums[(i+1)%n]) count++;
+    }
+    return count<=1;
+};

40cefeb9da422b11c683e1eeafc702996a648665
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-07-07 21:35:09 +0530

    Time: 38 ms (47.00%) | Memory: 71.8 MB (78.02%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(arr) {
+    let ans = [];
+    let n = arr.length;
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < n; i++) {
+        
+        if (i !== 0 && arr[i] === arr[i - 1]) continue;
+
+        
+        let j = i + 1;
+        let k = n - 1;
+        while (j < k) {
+            let sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
+            } else {
+                let temp = [arr[i], arr[j], arr[k]];
+                ans.push(temp);
+                j++;
+                k--;
+                
+                while (j < k && arr[j] === arr[j - 1]) j++;
+                while (j < k && arr[k] === arr[k + 1]) k--;
+            }
+        }
+    }
+    return ans;
+};

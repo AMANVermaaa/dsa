7de5433bd9e64ecce52385f0a3781687946d920c
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-08-02 13:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (45.43%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -19,25 +19,56 @@ class Solution {
         // postorder(root, ls);
         // return ls;
 
-        List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> s1 = new Stack<TreeNode>();
-        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        // List<Integer> list = new ArrayList<>();
+        // Stack<TreeNode> s1 = new Stack<TreeNode>();
+        // Stack<TreeNode> s2 = new Stack<TreeNode>();
 
-        if(root == null) return list;
-        s1.push(root);
-        while(!s1.isEmpty()){
-            root=s1.pop();
-            s2.push(root);
-            if(root.left != null) s1.push(root.left);
-            if(root.right != null) s1.push(root.right);
+        // if(root == null) return list;
+        // s1.push(root);
+        // while(!s1.isEmpty()){
+        //     root=s1.pop();
+        //     s2.push(root);
+        //     if(root.left != null) s1.push(root.left);
+        //     if(root.right != null) s1.push(root.right);
 
+        // }
+
+        // while(!s2.isEmpty()){
+        //     list.add(s2.pop().val);
+
+        // }
+        // return list;
+
+
+        Stack<TreeNode> s1=new Stack<TreeNode>();
+        List<Integer> post= new ArrayList<>();
+
+        TreeNode curr=root;
+        TreeNode temp=null;
+
+        while(curr!=null || !s1.isEmpty()){
+            if(curr!=null){
+                s1.push(curr);
+                curr=curr.left;
+            }
+            else{
+                temp=s1.peek().right;
+                if(temp==null){
+                    temp=s1.pop();
+                    
+                    post.add(temp.val);
+                    while(!s1.isEmpty() && temp==s1.peek().right){
+                        temp = s1.pop();
+                        
+                        post.add(temp.val);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
         }
-
-        while(!s2.isEmpty()){
-            list.add(s2.pop().val);
-
-        }
-        return list;
+        return post;
     }
     // public void postorder(TreeNode root, List<Integer> ls){
     //     if(root == null ) return;

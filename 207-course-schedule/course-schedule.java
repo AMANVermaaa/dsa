9b294375430b962797886b9f9a7eae2e53a63b83
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-09-16 15:57:53 +0530

    Time: 851 ms (5.07%) | Memory: 219.5 MB (5.94%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int V, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<Integer>());
+        }
+
+        for(int i=0;i<V;i++){
+            for(int[] p: prerequisites){
+                adj.get(p[1]).add(p[0]);
+            }
+        }
+
+        int[] indegree= new int[V];
+        for(int i=0;i<V;i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
+
+
+        ArrayList<Integer> res = new ArrayList<>();
+        int cnt=0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            res.add(node);
+            cnt++;
+
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.offer(it);
+            }
+        }
+
+        if(cnt==V) return true;
+        return false;
+    }
+}

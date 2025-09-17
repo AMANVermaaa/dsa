4f442dff03a943c4c2823a1dd1564e5f9413235f
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-09-17 22:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (5.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] dp= new int[n+1];
+        if(n==1) return 1;
+        fib(n,dp);
+        return dp[n];
+
+    }
+    public int fib(int n, int[] dp){
+        
         if(n<=1) return 1;
+        
+        if(dp[n] != 0) return dp[n];
 
-        int prev2=1;
-        int prev=1;
-        int curr=1;
-        for(int i=2;i<=n;i++){
-            curr=prev2+prev;
-            prev2=prev;
-            prev=curr;
-        }
-        return prev;
-
+        return dp[n] = fib(n-1, dp) + fib(n-2, dp);
     }
 }

571bbc218cf7a0c2d8314deddb9de592b552c984
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-09-22 17:46:32 +0530

    Time: 6 ms (22.98%) | Memory: 52.6 MB (9.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,52 +3,53 @@ class Solution {
         int first;
         int second;
         pair(int first, int second){
-            this.first=first;
-            this.second=second;
+            this.first = first;
+            this.second = second;
         }
     }
+
     public void bfs(int row, int col, int[][] vis, char[][] grid){
+
+        int m = grid.length;
+        int n = grid[0].length;
         vis[row][col]=1;
         Queue<pair> q = new LinkedList<>();
-        q.offer(new pair(row,col));
-
-        int n =grid.length;
-        int m= grid[0].length;
-
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
+        q.offer(new pair(row, col));
+        
+        int[] drow= {-1,0,1,0};
+        int[] dcol= {0,-1,0,1}; 
         while(!q.isEmpty()){
-            int ro= q.peek().first;
-            int co= q.peek().second;
+            int nrow= q.peek().first;
+            int ncol= q.peek().second;
+
             q.remove();
 
-            for(int i=0;i<4;i++){
-                
-                int nrow=ro+drow[i];
-                int ncol=co+dcol[i];
+            for(int i =0;i<4;i++){
+                int ro = nrow +drow[i];
+                int co = ncol +dcol[i];
 
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m 
-                && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1' ){
-                    vis[nrow][ncol]=1;
-                    q.offer(new pair(nrow,ncol));
+                if(ro>=0 && co>=0 && ro<m && co<n && vis[ro][co]==0 && grid[ro][co]=='1'){
+                    vis[ro][co]=1;
+                    q.offer(new pair(ro,co));
                 }
-                
             }
+
+
         }
-        
+
     }
+    
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        int[][] vis = new int[n][m];
+        int[][] vis = new int[m][n];
         int count=0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-
-                if(vis[row][col]==0 && grid[row][col]=='1'){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j] == 0 && grid[i][j]=='1'){
                     count++;
-                    bfs(row, col, vis, grid);
+                    bfs(i,j,vis,grid);
                 }
             }
         }

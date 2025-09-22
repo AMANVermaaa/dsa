b8677df53dcfcb967460b80bb510c135952986b2
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-09-22 22:27:11 +0530

    Time: 2 ms (65.72%) | Memory: 42.9 MB (14.72%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,72 +1,67 @@
 class Solution {
-    class pair{
+    class triple{
         int first;
         int second;
         int time;
-        pair(int first, int second, int time){
-            this.first=first;
-            this.second=second;
-            this.time=time;
+        public triple(int first, int second, int time){
+            this.first = first;
+            this.second = second;
+            this.time = time;
         }
     }
-    public void bfs(int sr, int sc, int[][] vis, int[][] grid){
-        
-        
-
-    }
+    
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] vis = new int[n][m];    
         
-        Queue<pair> q = new LinkedList<>();
-        int cntFresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-                    q.offer(new pair(i,j,0));
+        int[][] vis = new int[m][n];
+        
+        int cntFresh=0;
+        Queue<triple> q = new LinkedList<>();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.offer(new triple(i,j,0));
                     vis[i][j]=2;
                 }
-                else{
+                else {
                     vis[i][j]=0;
                 }
 
                 if(grid[i][j]==1) cntFresh++;
             }
         }
-    
-        int time=0;
-        int[] drow= {-1,0,1,0};
-        int[] dcol= {0,-1,0,1};
 
-        int count=0;
+        int time = 0;
+        int count = 0;
 
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,-1,0,1};
         while(!q.isEmpty()){
-            int row= q.peek().first;
-            int col= q.peek().second;
-            int t= q.peek().time;
-            time= Math.max(time,t);
+            int row = q.peek().first;
+            int col = q.peek().second;
+            int t = q.peek().time;
+            time = Math.max(t,time);
+
             q.poll();
 
-            
             for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
                     vis[nrow][ncol]=2;
-                    q.offer(new pair(nrow, ncol, time+1));
+                    q.offer(new triple(nrow, ncol, time+1));
                     count++;
                 }
+                
             }
 
-
         }
-        if(count!=cntFresh) return -1;
-        return time;
-        
 
+        if(count == cntFresh) return time;
+        return -1;
     }
 }

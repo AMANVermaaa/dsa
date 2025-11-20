4fd11395eca692bfbef54c15451de4d8d52329db
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2025-11-20 23:42:36 +0530

    Time: 7 ms (50.46%) | Memory: 47.4 MB (7.14%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,38 @@
 class Solution {
-    public boolean canFinish(int V, int[][] prerequisites) {
-        
-        List<List<Integer>> adj = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0;i<V;i++){
-            adj.add(new ArrayList<Integer>());
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
         }
-
-        int m = prerequisites.length;
-        for(int i=0;i<m;i++){
+        for(int i=0;i<prerequisites.length;i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
 
-        int[] indegree=new int[V];
-        for(int i=0;i<V;i++){
-            for(int it: adj.get(i)){
-                indegree[it]++;
+        for(int i=0;i<numCourses;i++){
+            for(int neighbor: adj.get(i)){
+                indegree[neighbor]++;
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0)
-                q.offer(i);
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.offer(i);
         }
+        int visited=0;
 
-        List<Integer> topo = new ArrayList<>();
         while(!q.isEmpty()){
             int node = q.poll();
-            topo.add(node);
+            visited++;
 
-            for(int it: adj.get(node)){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.offer(it);
-                }
+            for(int neighbor: adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor]==0) q.offer(neighbor);
             }
         }
 
-        if(topo.size()==V) return true;
+        if(visited == numCourses) return true;
         return false;
-
     }
 }

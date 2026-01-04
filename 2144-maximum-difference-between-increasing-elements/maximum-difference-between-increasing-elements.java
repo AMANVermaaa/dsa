dfc4dd08b6de82ef9e2d8a6a80bce12b54bb715f
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-01-04 17:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (6.81%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,24 +1,11 @@
 class Solution {
     public int maximumDifference(int[] nums) {
-        // int diff= -1;
-        // for(int i=0;i<nums.length-1;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         int dif= nums[j]-nums[i];
-        //         if(dif>0 && dif>diff){
-        //             diff=dif;
-        //         }
-        //     }
-        // }
-        // return diff;
+        int diff=-1;
+        int min=nums[0];
 
-        int min=nums[0], diff=-1;
         for(int i=1;i<nums.length;i++){
-            if(min<nums[i]){
-                diff=Math.max(diff,nums[i]-min);
-            }
-            else{
-                min=nums[i];
-            }
+            if(min<nums[i]) diff=Math.max(diff, nums[i]-min);
+            else min=nums[i];
         }
         return diff;
     }

ded118ccc289fa8a8532cbdcca17f78f0dd7ba75
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-01-07 20:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return modifiedBinarySearch(nums,0,nums.length-1,target);
+        int low = 0, high = nums.length-1;
 
-    }
-    int modifiedBinarySearch(int arr[],int low, int high, int target){
-        if(low>high)
-            return -1;
-
-        int mid=low+(high-low)/2;
-        if(arr[mid]==target) return mid;
-        if(arr[low]<=arr[mid]){
-            if(arr[low]<=target && target<=arr[mid])
-                return modifiedBinarySearch(arr,low,mid-1,target);
-            else
-                return modifiedBinarySearch(arr,mid+1,high,target);
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
+            }
         }
-
-        else{
-            if(arr[mid]<=target && target<=arr[high])
-                return modifiedBinarySearch(arr,mid+1,high,target);
-            else 
-                return modifiedBinarySearch(arr,low,mid-1,target);
-        }
+        return -1;
     }
 }

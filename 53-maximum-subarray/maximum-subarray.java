b30492163434e306b9b9bc61f96bca58682f2664
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-02-27 13:54:17 +0530

    Time: 1 ms (99.73%) | Memory: 77.4 MB (47.46%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,8 +5,8 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(maxi<sum) maxi = sum;
-            if(sum<0) sum=0;
+            maxi = Math.max(maxi,sum);
+            sum = Math.max(0,sum);
         }
         return maxi;
     }

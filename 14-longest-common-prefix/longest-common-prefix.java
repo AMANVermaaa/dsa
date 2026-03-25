1a5fc206f2e1e79e32f833d322423a13c6ed6944
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-03-25 19:07:36 +0530

    Time: 1 ms (63.95%) | Memory: 43 MB (94.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs==null || strs.length==0) return "";
+        if(strs.length==0) return "";
 
         for(int i=0;i<strs[0].length();i++){
             char c = strs[0].charAt(i);

98cd84ac7512733fb4f59fe577408b625e1581d9
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-03-25 17:21:08 +0530

    Time: 5 ms (55.13%) | Memory: 52.9 MB (26.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
-        int cand1 = -1;
-        int vote1 = 0;
-        int cand2 = -1;
-        int vote2 = 0;
+        List<Integer> list = new ArrayList<>();
 
-        for(int num: nums){
-            if(cand1 == num) vote1++;
-            else if(cand2 == num) vote2++;
-            else if(vote1 == 0) {cand1 = num; vote1++;}
-            else if(vote2 == 0) {cand2 = num; vote2++;}
-            else {vote1--; vote2--;}
+        int v1 =0;
+        int cand1=0;
+        int v2= 0;
+        int cand2=0;
+
+        for(int num:nums){
+            if(cand1==num) v1++;
+            else if(cand2==num) v2++;
+            else if(v1==0) {cand1=num;v1=1;}
+            else if(v2==0) {cand2=num;v2=1;}
+            else {v1--; v2--;}
         }
 
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int cnt1=0;
+        int cnt2=0;
 
-        for(int num: nums){
-            if(cand1 == num) cnt1++;
-            else if(cand2 == num) cnt2++;
+        for(int num:nums){
+            if(num==cand1) cnt1++;
+            else if(num==cand2) cnt2++;
         }
-        
-        List<Integer> res = new ArrayList<>();
+        System.out.println(cand1);
+                System.out.println(cand2);
+        if(cnt1>nums.length/3) list.add(cand1);
+        if(cnt2>nums.length/3) list.add(cand2);
 
-        if(cnt1>nums.length/3) res.add(cand1);
-        if(cnt2>nums.length/3) res.add(cand2);
-
-        return res;
+        return list;
     }
 }

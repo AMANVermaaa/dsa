8367701810f975e884a0e0e8708063228e8c1487
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-06-14 14:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (31.42%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,37 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int index = -1;
-        // Step 1: Find the first decreasing element from the end
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        
+        int n= nums.length;
+
+        int i =n-1;
+
+        while(i>0){
+            if(nums[i] > nums[i-1]){
                 break;
             }
+            i--;
         }
-        // If no such element exists, reverse the whole array
-        if (index == -1) {
-            reverse(nums, 0, n - 1);
+
+        if(i==0){
+            reverse(nums, i, n-1);
             return;
         }
-        // Step 2: Find the next larger element to swap with
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums, i, index);
+
+        int j = n-1;
+        while(j>=i){
+            if(nums[j]>nums[i-1]){
                 break;
             }
+            j--;
         }
-        // Step 3: Reverse the part after index
-        reverse(nums, index + 1, n - 1);
+
+        swap(nums, i-1, j);
+        reverse(nums,i,n-1);
+
     }
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
-    private void reverse(int[] arr, int left, int right) {
-        while (left < right) {
-            swap(arr, left, right);
+    public void reverse(int[] nums, int left, int right){
+        while(left<=right){
+            swap(nums, left, right);
             left++;
             right--;
         }

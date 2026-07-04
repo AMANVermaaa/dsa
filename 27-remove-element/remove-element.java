f641859441b0ed47260d74b83b03a2c40154fdc7
Aman Verma <66158603+AMANVermaaa@users.noreply.github.com>
2026-07-04 17:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (95.54%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,16 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i=0;
-        int j=nums.length-1;
-        while(i<=j){
-            if(nums[i]==val) {swap(nums,i,j);j--;}
-            else i++;
+        
+        int i = 0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+            }
         }
         return i;
-    }
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
